cccc05c78b87184c2e7b6fd849bc11c278d83ccf
YuranyUlchur <ulchuryurani80@gmail.com>
2023-04-08 10:56:14 -0500

    add functions to export data in the CSV format, to export data in the JSON format and add in dictionary

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,51 +3,22 @@
 Gather data from an API
 """
 import requests
-import sys
-
-
-def get_employee_todo_progress(employee_id):
-    """
-    returns information about his/her TODO list progress.
-    """
-
-    """
-    Perform GET request to the API to obtain the list of tasks
-    """
-    link = (f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
-    todos_response = requests.get(link)
-    todos = todos_response.json()
-
-    """
-    Filter completed tasks
-    """
-    completed_tasks = [todo for todo in todos if todo['completed']]
-
-    """
-    Counts number of completed tasks and total number of tasks
-    """
-    number_of_done_tasks = len(completed_tasks)
-    total_number_of_tasks = len(todos)
-
-    """
-    obtain employee information
-    """
-    link2 = (f'https://jsonplaceholder.typicode.com/users/{employee_id}')
-    employee_response = requests.get(link2)
-    employee = employee_response.json()
-
-    """Get employee name"""
-    employee_name = employee['name']
-
-    print("Employee {} is done with tasks({}\{}):".format(employee_name, number_of_done_tasks, total_number_of_tasks))
-
-    """Print title of completed tasks"""
-    for task in completed_tasks:
-        print('\t', task['title'])
+from sys import argv
 
 
 if __name__ == "__main__":
-    """Prompts the user for the employee's ID"""
-    employee_id = (int(sys.argv[1]))
+    emp_id = argv[1]
 
-    get_employee_todo_progress(employee_id)
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/", params={"id": emp_id}).json()
+    tasks = requests.get(url + "todos/", params={"userId": emp_id}).json()
+
+    name = user[0].get("name") if len(user) > 0 else None
+    tasks_d = [task.get('title') for task in tasks
+               if task.get('completed') is True]
+    tasks_t, tasks_c = len(tasks), len(tasks_d)
+
+    print("Employee {} is done with tasks({}/{}):".format(name,
+                                                          tasks_c,
+                                                          tasks_t))
+    [print("\t {}".format(task)) for task in tasks_d]
